e0b5846623b74bc510fb9d9cd838a441739a68c4
Ismatulla Mansurov <ismatullamansurov@gmail.com>
2025-08-24 23:57:37 -0600

    fix: download directly

--- src/routes/wallpapers/[id]/+page.svelte
+++ src/routes/wallpapers/[id]/+page.svelte
@@ -104,7 +104,6 @@
         const link = document.createElement("a");
         link.href = url;
         link.download = filename;
-        link.target = "_blank";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
